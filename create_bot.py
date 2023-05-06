a44d9d044ade7559054c3fa450f36fa1578f3768
BohdanOnukevich <onukevich22@gmail.com>
2023-05-07 00:43:11 +0300

    add config

--- create_bot.py
+++ create_bot.py
@@ -1,12 +1,13 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+from config import TOKEN
 
-TOKEN = os.getenv('TOKEN')
+# load_dotenv()
+
+# TOKEN = os.getenv('TOKEN')
 
 bot = Bot(TOKEN)
 storage = MemoryStorage()
