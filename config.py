09cea6539a4e8e9b05292f9608bf51b3d70dedd6
BohdanOnukevich <onukevich22@gmail.com>
2023-05-20 00:02:04 +0300

    FishCountBot v0.6.3

--- config.py
+++ config.py
@@ -4,9 +4,7 @@ import os
 load_dotenv()
 
 
-# TOKEN = os.environ.get("TOKEN")
+TOKEN = os.environ.get("TOKEN")
 
-# DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-TOKEN='6006030260:AAHNjf5sqS58G0h6o7gxFZL9x7Zos4bVehE'
-DATABASE_URL='postgresql+asyncpg://inquisitor:j9IaPi8zBx3XVf54diTG4GSJwXg0NkE0@dpg-chj68cvdvk4poot7huc0-a.oregon-postgres.render.com:5432/fishcountbotdb'
